742bb4d63fc7eb812c6fc8dcb6b35b36dd33f094
Jonathan Nelson <ciasaboark@gmail.com>
2014-10-06 15:39:00 -0400

    Stripping local database to minimum values
    Columns:
    -local database now only stores the:
      -instance id
      -event id
      -calendar id
      -ringer type
    
    EventDatabaseOpenHelper:
    -bumped up version number to 7
    -changed database creation to only use the minimum values from Columns

--- tacere/src/main/java/org/ciasaboark/tacere/database/EventDatabaseOpenHelper.java
+++ tacere/src/main/java/org/ciasaboark/tacere/database/EventDatabaseOpenHelper.java
@@ -12,7 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 class EventDatabaseOpenHelper extends SQLiteOpenHelper {
     public static final String TABLE_EVENTS = "events";
     private static final String DB_NAME = "events.sqlite";
-    private static final int VERSION = 4;
+    private static final int VERSION = 7;
 
     public EventDatabaseOpenHelper(Context context) {
         super(context, DB_NAME, null, VERSION);
@@ -25,11 +25,9 @@ class EventDatabaseOpenHelper extends SQLiteOpenHelper {
 
     private void createTable(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_EVENTS + " ( " + Columns._ID + " integer primary key," +
-                Columns.EVENT_ID + " integer, " + Columns.TITLE + " varchar(100), " +
-                Columns.DESCRIPTION + " varchar(100), " + Columns.BEGIN + " integer, " +
-                Columns.END + " integer, " + Columns.IS_ALLDAY + " integer, " +
-                Columns.IS_FREETIME + " integer, " + Columns.RINGER_TYPE + " integer, " +
-                Columns.DISPLAY_COLOR + " integer, " + Columns.CAL_ID + " integer " +
+                Columns.EVENT_ID + " integer, " +
+                Columns.RINGER_TYPE + " integer, " +
+                Columns.CAL_ID + " integer" +
                 ")");
     }
 
