b93008cfda7f71b1828a49d9f664627d31cc95d0
Jonathan Nelson <ciasaboark@gmail.com>
2015-12-12 00:41:14 -0500

    Pulling out github oauth key

---
+++ tacere/src/main/java/org/ciasaboark/tacere/keyset/GitHubKeySet.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 Jonathan Nelson
+ * Released under the BSD license.  For details see the COPYING file.
+ */
+
+package org.ciasaboark.tacere.keyset;
+
+/**
+ * Created by Jonathan Nelson on 12/12/15.
+ */
+public class GitHubKeySet {
+    public static final String GITHUB_OAUTH = "";
+}
