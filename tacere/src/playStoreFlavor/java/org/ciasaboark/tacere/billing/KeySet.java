6413e6aa3cb17147517a0a62210bea90ed5c3f91
Jonathan Nelson <ciasaboark@gmail.com>
2015-12-12 01:09:58 -0500

    Move IAB keys to new flavor

---
+++ tacere/src/playStoreFlavor/java/org/ciasaboark/tacere/billing/KeySet.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 Jonathan Nelson
+ * Released under the BSD license.  For details see the COPYING file.
+ */
+
+package org.ciasaboark.tacere.billing;
+
+public class KeySet {
+    public static final String GOOGLE_LICENSE_KEY = "";
+    public static final String SKU_UPGRADE_PRO = "";
+
+    public static final String SKU_GOOGLE_TESTING = "";
+}
