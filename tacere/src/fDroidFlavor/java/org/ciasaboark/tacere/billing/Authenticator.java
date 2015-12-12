25da7fd6bb52d0e7152f5c9fc4753312ea1d9107
Jonathan Nelson <ciasaboark@gmail.com>
2015-12-12 01:14:56 -0500

    Separating FDroid and Google Play Store flavors

---
+++ tacere/src/fDroidFlavor/java/org/ciasaboark/tacere/billing/Authenticator.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015 Jonathan Nelson
+ * Released under the BSD license.  For details see the COPYING file.
+ */
+
+package org.ciasaboark.tacere.billing;
+
+import android.content.Context;
+
+/**
+ * Created by Jonathan Nelson on 12/12/15.
+ */
+public class Authenticator {
+    public Authenticator(Context ctx) {
+
+    }
+
+
+    public boolean isAuthenticated() {
+        return true;
+    }
+}
